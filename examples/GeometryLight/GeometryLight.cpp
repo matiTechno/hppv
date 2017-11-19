7287e57970ddf3860644c37dcae90d180b73207f
matiTechno <mateusz.macieja8@gmail.com>
2017-11-19 14:39:00 +0100

    .

--- examples/GeometryLight/GeometryLight.cpp
+++ examples/GeometryLight/GeometryLight.cpp
@@ -1,22 +1,19 @@
 #include <vector>
 #include <optional>
 #include <algorithm>
-#include <cassert>
 #include <cmath>
 
 #include <glm/trigonometric.hpp>
-#include <glm/gtc/constants.hpp>
 #include <glm/gtx/vector_angle.hpp>
 
 #include <hppv/App.hpp>
 #include <hppv/PrototypeScene.hpp>
 #include <hppv/Renderer.hpp>
 #include <hppv/glad.h>
+#include <hppv/imgui.h>
 
 std::optional<glm::vec2> findRayEnd(glm::vec2 rayStart, glm::vec2 rayPoint, glm::vec2 segStart, glm::vec2 segEnd)
 {
-    // todo: case when the ray is parallel to y-axis
-
     auto rayCoeff = (rayPoint.y - rayStart.y) / (rayPoint.x - rayStart.x);
 
     float x;
@@ -28,6 +25,10 @@ std::optional<glm::vec2> findRayEnd(glm::vec2 rayStart, glm::vec2 rayPoint, glm:
     else
     {
         auto segCoeff = (segEnd.y - segStart.y) / (segEnd.x - segStart.x);
+
+        if(segCoeff == rayCoeff)
+            return {};
+
         x = (segStart.y - segCoeff * segStart.x - rayStart.y + rayCoeff * rayStart.x) / (rayCoeff - segCoeff);
     }
 
@@ -37,115 +38,80 @@ std::optional<glm::vec2> findRayEnd(glm::vec2 rayStart, glm::vec2 rayPoint, glm:
     if(x < std::min(segStart.x, segEnd.x) || x > std::max(segStart.x, segEnd.x))
         return {};
 
-    return glm::vec2(x, rayCoeff * x + rayStart.y - rayCoeff * rayStart.x);
-}
+    glm::vec2 rayEnd(x, rayCoeff * x + rayStart.y - rayCoeff * rayStart.x);
 
-using hppv::Vertex;
+    if(rayEnd.y < std::min(segStart.y, segEnd.y) || rayEnd.y > std::max(segStart.y, segEnd.y))
+        return {};
+
+    return rayEnd;
+}
 
 class GeometryLight: public hppv::PrototypeScene
 {
 public:
     GeometryLight(): hppv::PrototypeScene({0.f, 0.f, 100.f, 100.f}, 1.1f, false)
     {
-        {
-            light_.center = {50.f, 50.f};
-            light_.radius = 1.f;
-            light_.color = {1.f, 0.f, 0.f, 1.f};
-        }
-        {
-            lineLoops_.emplace_back();
-            auto& loop = lineLoops_.back();
-
-            Vertex v1;
-            v1.pos = {30.f, 30.f};
-            Vertex v2;
-            v2.pos = {60.f, 40.f};
-            Vertex v3;
-            v3.pos = {50.f, 20.f};
-
-            loop.push_back(v1);
-            loop.push_back(v2);
-            loop.push_back(v3);
-        }
-        {
-            lineLoops_.emplace_back();
-            auto& loop = lineLoops_.back();
-
-            Vertex v1;
-            v1.pos = {70.f, 15.f};
-            Vertex v2;
-            v2.pos = {60.f, 25.f};
-            Vertex v3;
-            v3.pos = {65.f, 30.f};
-
-            loop.push_back(v1);
-            loop.push_back(v2);
-            loop.push_back(v3);
-        }
-        {
-            lineLoops_.emplace_back();
-            auto& loop = lineLoops_.back();
-
-            Vertex v1;
-            v1.pos = border_.pos;
-            Vertex v2;
-            v2.pos = border_.pos + glm::vec2(border_.size.x, 0.f);
-            Vertex v3;
-            v3.pos = border_.pos + border_.size;
-            Vertex v4;
-            v4.pos = border_.pos + glm::vec2(0.f, border_.size.y);
-
-            loop.push_back(v1);
-            loop.push_back(v2);
-            loop.push_back(v3);
-            loop.push_back(v4);
-        }
+        setLineLoops();
     }
 
 private:
-    std::vector<std::vector<hppv::Vertex>> lineLoops_;
+    std::vector<std::vector<glm::vec2>> lineLoops_;
     hppv::Space border_{10.f, 10.f, 80.f, 80.f};
-    hppv::Circle light_;
     std::vector<glm::vec2> points_;
+    glm::vec2 lightPos_;
+
+    struct
+    {
+        bool drawPoints = true;
+        bool drawLines = true;
+        bool drawTriangles = true;
+    }
+    options_;
+
+    void setLineLoops();
 
     void setPoints()
     {
         points_.clear();
 
-        for(auto& loop: lineLoops_)
+        for(const auto& loop: lineLoops_)
         {
-            for(int i = -1; i < 2; ++i)
+            // change to: i = -1; i < 2
+            for(int i = 0; i < 1; ++i)
             {
-                for(auto& vertex: loop)
+                for(auto point: loop)
                 {
-                    glm::vec2 point(666.f);
+                    std::optional<glm::vec2> rayEnd;
 
-                    for(auto& loop: lineLoops_)
+                    for(const auto& loop: lineLoops_)
                     {
-                        for(auto it = loop.begin(); it < loop.end(); ++it)
+                        for(auto it = loop.cbegin(); it < loop.cend(); ++it)
                         {
-                            auto segEnd = (it == loop.end() - 1 ? loop.begin()->pos : (it + 1)->pos);
-                            auto rayPos = glm::rotate(vertex.pos, i * 0.00001f);
+                            auto segEnd = (it == loop.end() - 1 ? *loop.begin() : *(it + 1));
+                            auto rayPoint = glm::rotate(point, i * 0.00001f);
 
-                            if(auto newPoint = findRayEnd(light_.center, rayPos, it->pos, segEnd))
+                            if(auto newRayEnd = findRayEnd(lightPos_, rayPoint, *it, segEnd))
                             {
-                                if(glm::length(*newPoint - light_.center) < glm::length(point - light_.center))
+                                if(!rayEnd || glm::length(*newRayEnd - lightPos_) < glm::length(*rayEnd - lightPos_))
                                 {
-                                    point = *newPoint;
+                                    rayEnd = *newRayEnd;
                                 }
                             }
                         }
                     }
 
-                    points_.push_back(point);
+                    if(rayEnd)
+                    {
+                        points_.push_back(*rayEnd);
+                    }
                 }
             }
         }
 
-        std::sort(points_.begin(), points_.end(), [this](const glm::vec2& l, const glm::vec2& r)
+        std::sort(points_.begin(), points_.end(), [lightPos = lightPos_](const glm::vec2& l, const glm::vec2& r)
         {
-            return std::atan2(l.x - light_.center.x, l.y - light_.center.y) <
-                   std::atan2(r.x - light_.center.x, r.y - light_.center.y);
+            return std::atan2(l.x - lightPos.x, l.y - lightPos.y) <
+                   std::atan2(r.x - lightPos.x, r.y - lightPos.y);
         });
     }
 
@@ -153,15 +119,11 @@ private:
     {
         if(isInput)
         {
-            auto pos = hppv::mapCursor(frame_.cursorPos, space_.projected, this);
-
-            if(pos.x - light_.radius > border_.pos.x &&
-               pos.y - light_.radius > border_.pos.y &&
-               pos.x + light_.radius < border_.pos.x + border_.size.x &&
-               pos.y + light_.radius < border_.pos.y + border_.size.y)
-            {
-                light_.center = pos;
-            }
+            lightPos_ = hppv::mapCursor(frame_.cursorPos, space_.projected, this);
+            lightPos_.x = std::max(lightPos_.x, border_.pos.x + 1);
+            lightPos_.y = std::max(lightPos_.y, border_.pos.y + 1);
+            lightPos_.x = std::min(lightPos_.x, border_.pos.x + border_.size.x - 1);
+            lightPos_.y = std::min(lightPos_.y, border_.pos.y + border_.size.y - 1);
         }
 
         setPoints();
@@ -169,46 +131,79 @@ private:
 
     void prototypeRender(hppv::Renderer& renderer) override
     {
+        ImGui::Begin("light options", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+        {
+            ImGui::Checkbox("draw points", &options_.drawPoints);
+            ImGui::Checkbox("draw lines", &options_.drawLines);
+            ImGui::Checkbox("draw triangles", &options_.drawTriangles);
+        }
+        ImGui::End();
+
         renderer.mode(hppv::Mode::Vertices);
         renderer.shader(hppv::Render::VerticesColor);
 
-        renderer.primitive(GL_TRIANGLES);
-
-        for(auto it = points_.begin(); it < points_.end(); ++it)
+        if(options_.drawTriangles)
         {
-            auto last = (it == points_.end() - 1 ? *points_.begin() : *(it + 1));
+            renderer.primitive(GL_TRIANGLES);
 
-            Vertex v;
-            v.color = {0.3f, 0.f, 0.f, 0.5f};
+            for(auto it = points_.cbegin(); it < points_.cend(); ++it)
+            {
+                auto last = (it == points_.end() - 1 ? *points_.begin() : *(it + 1));
 
-            v.pos = light_.center;
-            renderer.cache(v);
-            v.pos = *it;
-            renderer.cache(v);
-            v.pos = last;
-            renderer.cache(v);
+                hppv::Vertex v;
+                v.color = {0.3f, 0.f, 0.f, 0.5f};
+
+                v.pos = lightPos_;
+                renderer.cache(v);
+                v.pos = *it;
+                renderer.cache(v);
+                v.pos = last;
+                renderer.cache(v);
+            }
         }
 
         renderer.primitive(GL_LINE_LOOP);
 
-        for(auto& loop: lineLoops_)
+        for(const auto& loop: lineLoops_)
         {
-            for(auto& vertex: loop)
+            for(auto point: loop)
             {
-                renderer.cache(vertex);
+                hppv::Vertex v;
+                v.pos = point;
+                v.color = {0.5f, 0.5f, 0.5f, 1.f};
+                renderer.cache(v);
             }
 
             renderer.breakBatch();
         }
-        for(auto& point: points_)
+
+        if(options_.drawLines)
         {
-            Vertex v;
-            v.pos = point;
-            v.color = {1.f, 0.f, 0.f, 1.f};
-            renderer.cache(v);
-            v.pos = light_.center;
-            renderer.cache(v);
-            renderer.breakBatch();
+            for(auto point: points_)
+            {
+                hppv::Vertex v;
+                v.pos = point;
+                v.color = {1.f, 0.f, 0.f, 1.f};
+                renderer.cache(v);
+                v.pos = lightPos_;
+                renderer.cache(v);
+                renderer.breakBatch();
+            }
+        }
+
+        if(options_.drawPoints)
+        {
+            renderer.mode(hppv::Mode::Instances);
+            renderer.shader(hppv::Render::CircleColor);
+
+            for(auto point: points_)
+            {
+                hppv::Circle c;
+                c.center = point;
+                c.radius = 0.5f;
+                c.color = {1.f, 0.f, 0.f, 1.f};
+                renderer.cache(c);
+            }
         }
     }
 };
@@ -220,4 +215,46 @@ int main()
     app.pushScene<GeometryLight>();
     app.run();
     return 0;
+}
+
+void GeometryLight::setLineLoops()
+{
+    {
+        lineLoops_.emplace_back();
+        auto& loop = lineLoops_.back();
+        loop.emplace_back(30.f, 30.f);
+        loop.emplace_back(60.f, 40.f);
+        loop.emplace_back(50.f, 20.f);
+    }
+    {
+        lineLoops_.emplace_back();
+        auto& loop = lineLoops_.back();
+        loop.emplace_back(70.f, 15.f);
+        loop.emplace_back(60.f, 25.f);
+        loop.emplace_back(65.f, 30.f);
+    }
+    {
+        lineLoops_.emplace_back();
+        auto& loop = lineLoops_.back();
+        loop.emplace_back(20.f, 60.f);
+        loop.emplace_back(40.f, 60.f);
+        loop.emplace_back(40.f, 75.f);
+        loop.emplace_back(30.f, 75.f);
+    }
+    {
+        lineLoops_.emplace_back();
+        auto& loop = lineLoops_.back();
+        loop.emplace_back(65.f, 40.f);
+        loop.emplace_back(78.f, 30.f);
+        loop.emplace_back(70.f, 50.f);
+        loop.emplace_back(65.f, 45.f);
+    }
+    {
+        lineLoops_.emplace_back();
+        auto& loop = lineLoops_.back();
+        loop.emplace_back(border_.pos);
+        loop.emplace_back(border_.pos + glm::vec2(border_.size.x, 0.f));
+        loop.emplace_back(border_.pos + border_.size);
+        loop.emplace_back(border_.pos + glm::vec2(0.f, border_.size.y));
+    }
 }
